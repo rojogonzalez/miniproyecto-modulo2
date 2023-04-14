4a71abdc2301e56697f563226f0fc873db265c56
rojogonzalez <r.gonzalez0193@gmail.com>
2023-04-13 21:43:06 -0600

    "Second commit"

--- windbnb-base-main/js/app.js
+++ windbnb-base-main/js/app.js
@@ -3,24 +3,9 @@ import dom from './dom.js'
 
 const datos = await data.getData();
 
-const hosts = dom.$('#hosts');
-
 datos.forEach(element => {
     const card = dom.newCard(element);
-    hosts.appendChild(card);
-
-    let superHost = element.superHost;
-    /*console.log(superHost);*/
-
-    if (superHost !== true) {
-        let padre = dom.$('.super-host').parentNode; //Guardo en la variable 'padre' el nodo padre del span class=super-host
-        console.log(element.title+"  "+superHost); //Reviso que se esté cumpliendo la condición y solo hayan superHost false
-        console.log(padre); //Verifico que el nodo almacenado en 'padre' sea el correcto
-        let a = dom.$('.super-host');
-        console.log(a);
-        padre.removeChild(a);
-        /*dom.$('.super-host').parentNode.removeChild(dom.$('.super-host'))*/
-    }       
+    hosts.appendChild(card);  
         
 });
 
--- windbnb-base-main/js/dom.js
+++ windbnb-base-main/js/dom.js
@@ -7,6 +7,7 @@ const $ = (selector) => document.querySelector(selector);
 
 const newCard = (obj) => {
     const div = document.createElement('div');
+    
     div.className += "col mb-4 border-0"
 
     div.innerHTML = `
@@ -14,21 +15,21 @@ const newCard = (obj) => {
     <img class="w-100 h-100 rounded-4 image-modified" src="${obj.photo}" alt="${obj.title}">
     </div>
 
-    <p class="d-flex justify-content-between mb-0">
-    <span>
-        <span class="border border-black rounded-pill px-2 py-1 text-light-emphasis super-host">SUPER HOST</span>
+    <section class="d-flex justify-content-between mb-0">
+    <div>
+        <span class=" ${!obj.superHost ? "hide" : ""} border border-black rounded-pill px-2 py-1 text-light-emphasis super-host">SUPER HOST</span>
         <span class="text-body-tertiary py-1 description">${obj.type}</span>
-    </span>
-    <span>
-        <span class="material-symbols-outlined star">star</span>
+    </div>
+    <div>
+        <span class="material-symbols-outlined text-danger star">star</span>
         <span class="text-light-emphasis py-2 rate">${obj.rating}</span>
-    </span>
-
-    </p>
+    </div>
+    </section>
     <p class="fw-semibold text-body-emphasis text-start py-1 card-title">
     ${obj.title}
     </p>
     `;
+
     return div;
 }
 
