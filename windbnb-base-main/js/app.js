a51233f8bd391309c5bcdc489baf090fcfb639d7
rojogonzalez <r.gonzalez0193@gmail.com>
2023-04-17 03:42:32 -0600

    Fifth commit

--- windbnb-base-main/js/app.js
+++ windbnb-base-main/js/app.js
@@ -11,41 +11,96 @@ const locationsList = [...dom.$('#locationsList').children];
 
 let activeLocation = 0;
 
+let contadorAdultos = 0;
+let sumaAdultos = dom.$('#sumaAdultos');
+
+let contadorNinos = 0;
+let sumaNinos = dom.$('#sumaNinos');
+
+let totalGuests = 0;
+let addGuests = dom.$('#addGuests');
+
+//Filtro por cantidad de Guests
+
+let addAdult = dom.$('#addAdult');
+addAdult.addEventListener('click', () => {
+    contadorAdultos++;
+    totalGuests++;
+    sumaAdultos.innerText = `${contadorAdultos}`;
+    addGuests.innerText = `${totalGuests == '0' ? 'Add guests' : totalGuests}`;
+});
+
+let subtractAdult = dom.$('#subtractAdult');
+subtractAdult.addEventListener('click', () => {
+    contadorAdultos--;
+    if (contadorAdultos < 0) contadorAdultos = 0;
+    totalGuests--;
+    if (totalGuests < 0) totalGuests = 0;
+    sumaAdultos.innerText = `${contadorAdultos}`;
+    addGuests.innerText = `${totalGuests == '0' ? 'Add guests' : totalGuests}`;
+})
+
+let addChild = dom.$('#addChild');
+addChild.addEventListener('click', () => {
+    contadorNinos++;
+    totalGuests++;
+    sumaNinos.innerText = `${contadorNinos}`;
+    addGuests.innerText = `${totalGuests == '0' ? 'Add guests' : totalGuests}`;
+});
+
+let subtractChild = dom.$('#subtractChild');
+subtractChild.addEventListener('click', () => {
+    contadorNinos--;
+    if (contadorNinos < 0) contadorNinos = 0;
+    totalGuests--;
+    if (totalGuests < 0) totalGuests = 0;
+    sumaNinos.innerText = `${contadorNinos}`;
+    addGuests.innerText = `${totalGuests == '0' ? 'Add guests' : totalGuests}`;
+})
+
 locationsList.forEach((loc, index) => {
 
     loc.addEventListener('click', () => {
-        if (loc.classList.contains('activeLocation')) return;
+        //if (loc.classList.contains('activeLocation')) return;
         loc.classList.add('activeLocation');
         locationsList[activeLocation].classList.remove('activeLocation');
         activeLocation = index
         let filtro = loc.children[1].textContent;
 
         //Filtrando usando loc
-        const filtered = filtro === 'Everywhere' ? datos : data.filtrar(datos, filtro);
-        console.log(filtered);
+        const filteredByLoc = filtro === 'Everywhere' ? datos : data.filtrar(datos, filtro);
 
-        let textBox = dom.$('#addLocation');
-        let textBox2 = dom.$('#addLocation2');
-        let textBox3 = dom.$('#staysIn')
-        textBox.textContent = `${filtro === "Everywhere" ? "Add location" : filtro}`;
-        textBox2.textContent = `${filtro === "Everywhere" ? "Add location" : filtro}`;
-        textBox3.textContent = `Stays in ${filtro === "Everywhere" ? "Everywhere" : filtro}`;
-        console.log(textBox3);
+        let filteredByGuests = [];
+        filteredByLoc.forEach((loc2) => {
 
-        const searchLocation = dom.$('#searchLocation');
-        searchLocation.addEventListener('click', () => {
-            dom.showCards(filtered);
-        })
+            if (loc2.maxGuests >= addGuests.innerText) filteredByGuests.push(loc2);
+            console.log(filteredByGuests);
+            //return filteredByGuests;
 
-        //dom.showCards(filtered);
+            console.log(filteredByGuests);
+
+            let textBox = dom.$('#addLocation');
+            let textBox2 = dom.$('#addLocation2');
+            let textBox3 = dom.$('#staysIn');
+            let textBox4 = dom.$('#stays');
+            let textBox5 = dom.$('#textBox5');
+            textBox.textContent = `${filtro === "Everywhere" ? "Add location" : filtro}`;
+            textBox2.textContent = `${filtro === "Everywhere" ? "Add location" : filtro}`;
+            textBox3.textContent = `Stays in ${filtro === "Everywhere" ? "Everywhere" : filtro}`;
+            textBox4.textContent = filteredByGuests.length;
+            textBox5.textContent = `${totalGuests == '0' ? 'Add guests' : totalGuests}`
+
+            const searchLocation = dom.$('#searchLocation');
+            searchLocation.addEventListener('click', () => {
+                if (addGuests.innerText === 'Add guests') {
+                    dom.showCards(filteredByLoc);
+                } else {
+                    dom.showCards(filteredByGuests);
+                };
+            })
+
+        });
+
     })
-
-
 })
-
-
-const cards = dom.showCards(datos);
-
-
-
 
